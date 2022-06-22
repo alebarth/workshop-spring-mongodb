336a0af4ac47065c0ec9e8f7d15b8dc4bcdc572d
Alexandre Barth <alebarth2001@gmail.com>
2022-06-22 20:00:22 -0300

    Obtendo um usuario por id

--- src/main/java/com/alebarth/workshopmongodb/services/UserService.java
+++ src/main/java/com/alebarth/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.alebarth.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alebarth.workshopmongodb.domain.User;
 import com.alebarth.workshopmongodb.repository.UserRepository;
+import com.alebarth.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
